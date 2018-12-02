1cbec091cb2e17bee8a7c0126235fe7ef93b2885
UpcraftLP <j-schirra@gmx.de>
2018-12-02 15:05:05 +0100

    day 1

--- src/com/github/upcraftlp/aoc/Main.java
+++ src/com/github/upcraftlp/aoc/Main.java
@@ -1,8 +1,10 @@
 package com.github.upcraftlp.aoc;
 
+import com.github.upcraftlp.aoc.days.Day1;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        new Day1().doTasks();
     }
 }
